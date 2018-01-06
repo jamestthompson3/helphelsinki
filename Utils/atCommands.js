dd6806202e98fc0c4f9cff49d0446d4a95ff492a
jamestthompson3 <jamestthompson3@gmail.com>
2018-01-06 21:30:02 +0200

    [+] Adds missing deps

--- Utils/atCommands.js
+++ Utils/atCommands.js
@@ -1,10 +1,21 @@
 const _ = require('lodash'),
-      request = require('superagent')
+      request = require('superagent'),
+      Lokka = require('lokka').Lokka,
+      Transport = require('lokka-transport-http').Transport
 
 const detectCommand = _.flow(
   message => message.split('@'),
   messageArray => COMMANDS[messageArray[1]] == null ? directionsFunction(messageArray) : COMMANDS[messageArray[1]]()
   )
+
+const HSL_GRAPHQL_URL = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
+const graphQLClient = new Lokka({
+  transport: new Transport(HSL_GRAPHQL_URL)
+})
+
+
+const bikingFunction = () => graphQLClient.query(`{nearest(lat: 60.1731473 lon: 24.9224112,filterByPlaceTypes:BICYCLE_RENT) {edges {node {id,place {id}distance}}`)
+.then(result => result)
 
 
 const eatFunction = console.log('eatfunction')
@@ -16,15 +27,3 @@ const COMMANDS = {
 }
 
 exports.detectCommand = detectCommand
-
-
-const HSL_GRAPHQL_URL = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
-const graphQLClient = new Lokka({
-  transport: new Transport(HSL_GRAPHQL_URL)
-})
-
-function refreshStationCache() {
-  graphQLClient.query(`{nearest(lat: 60.1731473 lon: 24.9224112,filterByPlaceTypes:BICYCLE_RENT) {edges {node {id,place {id}distance}}`).then(result => {
-    stationCache = result
-  })
-}
